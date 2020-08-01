cc1c813b9f532c9dbec11b2de6af648a6dd6f09f
tarachan291 <tarachan291@gmail.com>
2020-08-01 12:17:32 +0900

    Initial Commit

---
+++ ByteCoinTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoinTracker
+//
+//  Created by Taras Hanis on 2020/08/01.
+//  Copyright © 2020 Taras Hanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
